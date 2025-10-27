462867ca2122be585f5fedcb597fc56d87cdb64c
Christian de Botton <christiandebotton@Christians-MacBook-Pro.local>
2025-10-27 01:41:09 -0700

    lint svelte with stylelint

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -6,7 +6,7 @@ require("conform").setup({
 		rust = { "rustfmt", lsp_format = "fallback" },
 		javascript = { "prettierd", "prettier", stop_after_first = true },
 		typescript = { "eslint_d", "prettier" },
-		svelte = { "eslint_d", "prettier" },
+		svelte = { "eslint_d", "prettier", "stylelint" },
 	},
 	format_on_save = {
 		-- I recommend these options. See :help conform.format for details.
--- nvim/lua/plugins/lint.lua
+++ nvim/lua/plugins/lint.lua
@@ -15,7 +15,29 @@ require("lint").linters_by_ft = {
 }
 
 vim.api.nvim_create_autocmd({ "BufReadPost", "BufNewFile", "BufWritePost", "InsertLeave", "BufEnter", "TextChanged" }, {
-	callback = function()
-		require("lint").try_lint()
+	callback = function(args)
+		local lint = require("lint")
+		local opts = { ignore_errors = args.event ~= "BufWritePost" }
+
+		-- Find clients for monorepo
+		local clients = vim.lsp.get_clients({ bufnr = 0 })
+		local key, client = next(clients)
+		while key do
+			if client.workspace_folders then
+				for _, dir in pairs(client.workspace_folders) do
+					if vim.fs.relpath(dir.name, vim.api.nvim_buf_get_name(0)) then
+						opts.cwd = dir.name
+					end
+				end
+			elseif client.root_dir then
+				opts.cwd = client.root_dir
+			end
+			if opts.cwd then
+				break
+			end
+			key, client = next(clients, key)
+		end
+
+		lint.try_lint(nil, opts)
 	end,
 })
