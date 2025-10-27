f510f7e36788daddc1b2c3a38fde47e9f6a02b54
Christian de Botton <christiandebotton@Christians-MacBook-Pro.local>
2025-10-27 01:25:45 -0700

    add other syntaxes to treesitter

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -8,7 +8,7 @@ require("nvim-treesitter").setup({
 require("nvim-treesitter").install({ "javascript", "typescript", "svelte", "rust", "go", "lua", "css", "sql", "html" })
 
 vim.api.nvim_create_autocmd("FileType", {
-	pattern = { "go", "rust", "svelte", "typescript", "javascript", "lua", "python" },
+	pattern = { "go", "rust", "svelte", "typescript", "javascript", "lua", "python", "css", "sql", "html" },
 	callback = function()
 		vim.treesitter.start()
 	end,
