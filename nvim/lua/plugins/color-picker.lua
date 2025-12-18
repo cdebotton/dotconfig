2fb6b27c5dc1ea52ae7a449376c351c6525fc475
Christian de Botton <416880+cdebotton@users.noreply.github.com>
2025-12-18 12:02:59 -0800

    fix color picker

--- nvim/lua/plugins/color-picker.lua
+++ nvim/lua/plugins/color-picker.lua
@@ -7,9 +7,6 @@ end, { desc = "Open OKLCH color picker" })
 local ccc = require("ccc")
 
 ccc.setup({
-	pickers = { ccc.picker.css_oklch, ccc.picker.css_hex },
-	inputs = { ccc.input.oklch, ccc.input.hex },
-	outputs = { ccc.output.css_oklch, ccc.output.hex },
 	highlight_mode = "background",
 	highlighter = {
 		auto_enable = true,
